42c5d73028e5531c7ea39a9b1e8fd1ee789d7ea4
phoenix0310 <phoenix03101990@gmail.com>
2018-01-13 14:29:24 +0700

    up

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('pdfShow', ["firebase","pdf","ngm.ngDrive"])
+    angular.module('pdfShow', ["firebase","pdf",])
         .controller('AddController', AddController)
         .service('GetDataService', GetDataService)
         .component('pdfShow', {
@@ -14,21 +14,14 @@
         });
         
 
-   angular.module('ngm.ngDrive')
-        .provider('OauthService', ngDrive.Config)
-    .config(function (OauthServiceProvider) {
-        OauthServiceProvider.setScopes('https://www.googleapis.com/auth/drive');
-        OauthServiceProvider.setClientID('566153587947-h2a4n5jp906egkkcap6g33f6fo3ssr0g.apps.googleusercontent.com');
-    });
 
 
+    AddController.$inject = ["firebase",'$scope'];
 
-    AddController.$inject = ['DriveService','$scope'];
-
-    function AddController(DriveService,$scope) {
+    function AddController($scope) {
         var pdf = this;
         
-  $scope.fetchedFile = DriveService.children.list('0B_JmUVVSA4onRjZOTWRkZm1BVWc').data;
+  
 
    
 
@@ -62,8 +55,11 @@
 
   function pdfShowController($scope) {
 var $ctrl= this;
- $scope.pdfUrl = '/77.pdf';
-        
+ 
+
+
+
+
 
     };
 
